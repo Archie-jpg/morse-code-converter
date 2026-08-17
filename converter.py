f277e0e9866d0ab7e3a9ef0b7e9ee990eafdf3bc
archie-jpg <archieleppard@gmail.com>
2026-08-17 11:41:44 +0100

    Started on function to convert from morse code to plain text

--- converter.py
+++ converter.py
@@ -20,8 +20,8 @@ LETTERS_TO_MORSE_CODE = {"a": ". -", "b": "- . . .",
 
 def pt_to_mc(plain_text: str) -> str:
     """Takes in a string in plain text, checks that it can be converted, returning the resulting morse code if it can. If it cannot
-    be converted, throws a value error.
-    i.e "Hello world" is converted to 
+    be converted, raises a value error.
+    i.e "Hello world" is converted to ". . . .   .   . - . .   . - . .   - - -             . - -   - - -   . - .   . - . .   - . ."
 
     Args:
         plain_text (str): A string containing only the letters a-z, the numbers 1-9 and spaces in lower case
@@ -30,7 +30,7 @@ def pt_to_mc(plain_text: str) -> str:
         str: Morse code version of input string
         
     Raises:
-        ValuesError: If the string contains any invalid characters (i.e *). This first invalid character is contained within the 
+        ValueError: If the string contains any invalid characters (i.e *). This first invalid character is contained within the 
         error message
     """
     output_string: str = ""
@@ -41,3 +41,20 @@ def pt_to_mc(plain_text: str) -> str:
         except KeyError:
             raise ValueError(f"{letter} is not convertable to morse code.")
     return output_string
+
+def mc_to_pt(morse_code: str) -> str:
+    """Converts morse code to plain text. Raises an error if it is not valid morse code
+    i.e ". . . .   .   . - . .   . - . .   - - -             . - -   - - -   . - .   . - . .   - . ." is converted to "Hello world"
+
+    Args:
+        morse_code (str): A string in morse code
+
+    Returns:
+        str: The plain text the morse code represents
+        
+    Raises:
+        ValueError: If the string contains a morse code pattern that is not recognised. Error message contains unrecognised morse code string
+    """
+    output_string: str = ""
+    for character in morse_code:
+        pass
